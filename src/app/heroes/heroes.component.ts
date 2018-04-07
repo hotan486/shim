52aef1823486fcbb6943ead71e62486d8d026abb
LeeDongKee <eastflag@gmail.com>
2018-04-07 15:17:00 +0900

    4장 ngFor 반복문 사용하여 여러개 데이터 보여주기
    let 아이템 of 컬렉션 (es6 반복문 구문 사용)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../HEROES';
 
 @Component({
   selector: 'app-heroes',
@@ -12,11 +13,13 @@ export class HeroesComponent implements OnInit {
     name: 'Winstorm',
   };*/
 
-  hero = new Hero();
+  // hero = new Hero();
+
+  heroes = HEROES;
 
   constructor() {
-    this.hero.id = 1;
-    this.hero.name = 'WinStorm';
+    // this.hero.id = 1;
+    // this.hero.name = 'WinStorm';
   }
 
   ngOnInit() {
