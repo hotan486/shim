55b0ce73507715a6288d9e72df057ff853d13699
LeeDongKee <eastflag@gmail.com>
2018-04-07 17:25:33 +0900

    6장 서비스
    1) 등록(injectable), 등록시점은 providers에서 결정
    2) 주입: 생성자 주입

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '../hero';
 import {HEROES} from '../HEROES';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +15,21 @@ export class HeroesComponent implements OnInit {
   };*/
 
   // hero = new Hero();
-
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() {
+  // 2) 서비스를 생성자로 주입
+  constructor(private heroService: HeroService) {
     // this.hero.id = 1;
     // this.hero.name = 'WinStorm';
   }
 
   ngOnInit() {
+    // 서비스 호출
+    // 1) 잘못e된 방법: 의존성이 생긴다. 컴포넌트가 서비스를 제어
+    // const heroService = new HeroService();
+    // this.heroes = heroService.getHeroes();
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero) {
